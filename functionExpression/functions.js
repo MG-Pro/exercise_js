2359923a7669e01908a161e9985db16e0301d491
MG <mg84@bk.ru>
2017-07-06 16:40:52 +0300

    add function

--- functionExpression/functions.js
+++ functionExpression/functions.js
@@ -1,3 +1,21 @@
-/**
- * Created by MG on 06.07.2017.
- */
+var studentsAndPoints =
+  [
+    'Алексей Петров', 0,
+    'Ирина Овчинникова', 60,
+    'Глеб Стукалов', 30,
+    'Антон Павлович', 30,
+    'Виктория Заровская', 30,
+    'Алексей Левенец', 70,
+    'Тимур Вамуш', 30,
+    'Евгений Прочан', 60,
+    'Александр Малов', 0
+  ];
+
+var students = [];
+var points = [];
+
+var students = function (studentsAndPoints) {
+    console.log('Студент ' + studentsAndPoints[i] + ' набрал ' + studentsAndPoints[i + 1] + ' баллов');
+    return students();
+}
+
