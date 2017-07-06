415daa3b63a27257ab2d295ab95fe3aebfb16265
MG <mg84@bk.ru>
2017-07-06 23:27:27 +0300

    function Task 1, 2 complite

--- functionExpression/functions.js
+++ functionExpression/functions.js
@@ -1,3 +1,34 @@
-/**
- * Created by MG on 06.07.2017.
- */
+var studentsAndPoints =
+      [
+        'Алексей Петров', 0,
+        'Ирина Овчинникова', 60,
+        'Глеб Стукалов', 30,
+        'Антон Павлович', 30,
+        'Виктория Заровская', 30,
+        'Алексей Левенец', 70,
+        'Тимур Вамуш', 30,
+        'Евгений Прочан', 60,
+        'Александр Малов', 0
+      ];
+
+// Task #1
+
+var students = [];
+var points = [];
+
+students = studentsAndPoints.filter(function (number, i) {
+  return !(i % 2);
+});
+
+points = studentsAndPoints.filter(function (number, i) {
+  return i % 2;
+});
+
+//console.log(students);
+//console.log(points);
+
+// Task #2
+
+students.forEach(function (val, i, students) {
+  console.log('Студент ' + students[i] + ' набрал ' + points[i] + ' баллов');
+});
