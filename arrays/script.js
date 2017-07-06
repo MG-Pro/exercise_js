3ab822c367cb84b0400cba627bb143e9b550a9d1
MG <mg84@bk.ru>
2017-07-06 14:38:49 +0300

    Task 1-2 ok

--- arrays/script.js
+++ arrays/script.js
@@ -1,5 +1,7 @@
 // console.log(studentsAndPoints.length);
+
 // Task #1
+
 //var students = [];
 //var points = [];
 //for(var i = 0; i < studentsAndPoints.length; i++) {
@@ -20,6 +22,16 @@
 //    console.log()
 //}
 
+// Task #1-2
+
+for(var i = 0; i < studentsAndPoints.length; i++) {
+  if (i % 2 === 0) {
+    console.log('Студент ' + studentsAndPoints[i] + ' набрал ' + studentsAndPoints[i + 1] + ' баллов');
+  }
+}
+
+
+
 // Task #2
 
 //var maxPointIn = 0;
@@ -37,32 +49,32 @@
 
 // Task #3
 
-var newStudents = ['Николай Фролов', 0, 'Олег Боровой', 0];
-for (var i = 0; i < newStudents.length; i++) {
-  studentsAndPoints.push(newStudents[i]);
-}
-// console.log(studentsAndPoints);
-
-// Task #4
-
-for (var i = 0; i < studentsAndPoints.length; i++) {
-  for (var j = 0; j < newStudents.length; j++) {
-    if (studentsAndPoints[i] === newStudents[j] && typeof newStudents[j] === 'string') {
-      studentsAndPoints[i + 1] += 10;
-    }
-  }
-}
-console.log(studentsAndPoints);
-
-// Task #5
-
-for(var i = 0; i < studentsAndPoints.length; i++) {
-  if(studentsAndPoints[i] === 0) {
-    console.log(studentsAndPoints[i - 1]);
-    studentsAndPoints.splice(i - 1, 2);
-  }
-}
-console.log(studentsAndPoints);
+//var newStudents = ['Николай Фролов', 0, 'Олег Боровой', 0];
+//for (var i = 0; i < newStudents.length; i++) {
+//  studentsAndPoints.push(newStudents[i]);
+//}
+//// console.log(studentsAndPoints);
+//
+//// Task #4
+//
+//for (var i = 0; i < studentsAndPoints.length; i++) {
+//  for (var j = 0; j < newStudents.length; j++) {
+//    if (studentsAndPoints[i] === newStudents[j] && typeof newStudents[j] === 'string') {
+//      studentsAndPoints[i + 1] += 10;
+//    }
+//  }
+//}
+//console.log(studentsAndPoints);
+//
+//// Task #5
+//
+//for(var i = 0; i < studentsAndPoints.length; i++) {
+//  if(studentsAndPoints[i] === 0) {
+//    console.log(studentsAndPoints[i - 1]);
+//    studentsAndPoints.splice(i - 1, 2);
+//  }
+//}
+//console.log(studentsAndPoints);
 
 
 
