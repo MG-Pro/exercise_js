dc02ea21d1afbbaedeab051c67c2b999e3714030
MG <mg84@bk.ru>
2017-07-10 18:05:06 +0300

    prototype task 1

--- prototype/prototype.js
+++ prototype/prototype.js
@@ -1,1 +1,30 @@
+var studentsAndPoints =
+  [
+    'Алексей Петров', 0,
+    'Ирина Овчинникова', 60,
+    'Глеб Стукалов', 30,
+    'Антон Павлович', 30,
+    'Виктория Заровская', 30,
+    'Алексей Левенец', 70,
+    'Тимур Вамуш', 30,
+    'Евгений Прочан', 60,
+    'Александр Малов', 0
+  ];
 
+// Task #1
+
+function Student(name, point) {
+  this.name = name;
+  this.point  = point;
+  this.show = function () {
+    console.log('Студент %s набрал %s баллов', this.name, this.point);
+  };
+}
+
+// Task #2
+
+
+var StudentList = new Array(groupName, list)
+StudentList.groupName = groupName;
+  
+console.log(StudentList);
