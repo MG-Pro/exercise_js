15bd9c92f4bd11d3d6cff0a1587415c617216369
MG <mg84@bk.ru>
2017-07-14 18:00:20 +0300

    Task #2 com

--- EloquentJavascript/3_ProgramStructure/script.js
+++ EloquentJavascript/3_ProgramStructure/script.js
@@ -7,3 +7,10 @@ for(var i = 1, s = '#'; i <= 7; i++) {
 
 // Task #2 FizzBuzz
 
+for(var i = 1; i <= 100; i++) {
+  if(i % 3 == 0)
+    console.log('Fizz');
+  else if(i % 5 == 0)
+    console.log('Buzz');
+  else console.log(i)
+}
