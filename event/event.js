ad559ef501a2f3df4d936f4c2d57e69d3fea5760
mg84 <mg84@bk.ru>
2017-07-18 23:06:28 +0300

    event Task #2-1 in work

--- event/event.js
+++ event/event.js
@@ -1,3 +1,4 @@
+'use strict';
 // Task #1-1
 document.getElementById('buttonList').addEventListener('click', function (e) {
   console.log(`Была нажата ${e.target.innerHTML}`);
@@ -15,4 +16,25 @@ document.getElementById('buttonList2').addEventListener('click', function (e) {
     targetElem.style.backgroundColor = 'yellow';
   }
 });
+
 // Task #2-1
+document.body.addEventListener('click', function (e) {
+    var lists = document.querySelectorAll('.form-group ul > li');
+    for(var i = 0; i < lists.length; i++) {
+      if(e.target != lists[i]) {
+        continue;
+      }
+      lists[i].style.backgroundColor = 'transparent';
+    }
+});
+document.querySelector('.form-group ul').addEventListener('click', function (e) {
+  if(e.ctrlKey) {
+    e.target.style.backgroundColor = 'grey';
+  } else {
+    var lists = document.querySelectorAll('.form-group ul > li');
+    for(var i = 0; i < lists.length; i++) {
+      lists[i].style.backgroundColor = 'transparent';
+    }
+    e.target.style.backgroundColor = 'grey';
+  }
+});
