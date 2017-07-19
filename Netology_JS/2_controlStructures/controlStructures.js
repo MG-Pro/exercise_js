99e6e4f794941dabf3d6b2ef6d3313edf2d4b22d
MG <mg84@bk.ru>
2017-07-19 22:10:53 +0300

    net hw les complete event T2-1 complete

---
+++ Netology_JS/2_controlStructures/controlStructures.js
@@ -0,0 +1,81 @@
+// Task #1
+'use strict';
+
+var stockCount = 656;
+var orderCount = 700;
+
+if(orderCount > stockCount) {
+  console.log('На складе нет такого количества товаров');
+} else if (orderCount == stockCount){
+  console.log('Вы забираете весь товар c нашего склада!');
+} else {
+  console.log('Заказ оформлен');
+}
+
+// Task #2
+var place = 'Звезда смерти';
+var price;
+
+switch (place) {
+  case 'Луна':
+  price = 150;
+  break;
+  case 'Крабовидная туманность':
+    price = 250;
+    break;
+  case 'Галактика Туманность Андромеды':
+    price = 550;
+    break;
+  case 'Туманность Ориона':
+    price = 600;
+    break;
+  case 'Звезда смерти':
+    price = 'договорная цена';
+    break;
+  default:
+    price = 'В ваш квадрант доставка не осуществляется';
+}
+
+console.log(`Стоимость доставки для области ${place}: ${price} Q`);
+
+// Task #3
+
+var itemPrice = 'oo';
+
+try {
+  if(typeof +itemPrice != 'number')
+    throw `Вы допустили ошибку: ${itemPrice} не является числом`
+  else
+    throw 'Цена товара введена корректно';
+} catch (msg) {
+  console.log(msg);
+}
+
+// Task #4
+
+var age = 15;
+var residence = 'Земля';
+var msg;
+
+if(residence == 'Земля' && age < 18)
+  msg = 'Вы не достигли совершеннолетия';
+else if(residence == 'Земля' && age >= 18)
+  msg = 'Приятных покупок';
+else if(residence == 'Юпитер' && age < 120)
+  msg = 'Сожалеем. Вернитесь на 120-й день рождения!';
+else if(residence == 'Юпитер' && age >= 120)
+  msg = 'Чистого неба и удачных покупок!';
+else
+  msg = 'Спасибо, что пользуетесь услугами нашего магазина!';
+console.log(msg);
+
+
+
+
+
+
+
+
+
+
+
--- event/event.js
+++ event/event.js
@@ -8,33 +8,36 @@ document.getElementById('buttonList').addEventListener('click', function (e) {
 var targetElem;
 document.getElementById('buttonList2').addEventListener('click', function (e) {
   e.preventDefault();
-  if(targetElem)
+  if (targetElem)
     targetElem.style.backgroundColor = 'transparent';
 
-  if(e.target.tagName == 'A') {
+  if (e.target.tagName == 'A') {
     targetElem = e.target.parentNode;
     targetElem.style.backgroundColor = 'yellow';
   }
 });
 
 // Task #2-1
+var eventCount = false;
 document.body.addEventListener('click', function (e) {
+  if(e.target.nodeName != 'LI' && eventCount == true) {
     var lists = document.querySelectorAll('.form-group ul > li');
-    for(var i = 0; i < lists.length; i++) {
-      if(e.target != lists[i]) {
-        continue;
-      }
+    for (var i = 0; i < lists.length; i++) {
       lists[i].style.backgroundColor = 'transparent';
     }
+  }
 });
 document.querySelector('.form-group ul').addEventListener('click', function (e) {
-  if(e.ctrlKey) {
-    e.target.style.backgroundColor = 'grey';
+  if(e.target.nodeName != 'LI')
+    return;
+  if (e.ctrlKey) {
+    e.target.style.backgroundColor = '#9a9292';
   } else {
     var lists = document.querySelectorAll('.form-group ul > li');
-    for(var i = 0; i < lists.length; i++) {
+    for (var i = 0; i < lists.length; i++) {
       lists[i].style.backgroundColor = 'transparent';
     }
-    e.target.style.backgroundColor = 'grey';
+    e.target.style.backgroundColor = '#9a9292';
   }
+  eventCount = true;
 });
