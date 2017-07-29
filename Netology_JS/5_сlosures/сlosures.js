acd2667290ee84a32925130292b08a0950e03ba7
MG <mg84@bk.ru>
2017-07-29 22:48:12 +0300

    net hw 5 cmpl

--- Netology_JS/5_сlosures/сlosures.js
+++ Netology_JS/5_сlosures/сlosures.js
@@ -4,7 +4,7 @@ let taxCount = 0;
 const TAX_RATE = 73;
 
 function getTax(cost) {
-  taxCount += cost * TAX_RATE/100;
+  taxCount += cost * TAX_RATE / 100;
 }
 
 getTax(30000);
@@ -15,7 +15,10 @@ getTax(787453);
 console.log(`Налог с продаж (${TAX_RATE}%) к оплате: ${taxCount} Q`);
 
 // Task #2
+console.log('\n');
+
 let residue = 30;
+
 function availabilityWrap(width, height, length) {
   let area = 2 * (width * height + width * length + height * length);
 
@@ -29,7 +32,7 @@ function availabilityWrap(width, height, length) {
 
 function printResult(width, height, length) {
   let result = availabilityWrap(width, height, length);
-  if(result[0]) {
+  if (result[0]) {
     console.log(`Заказ (${result[1]}/${result[2]}/${result[3]} метра) упакован, осталось упаковочной бумаги ${residue} м2`);
   } else {
     console.log(`Заказ (${result[1]}/${result[2]}/${result[3]} метра) не упакован, осталось упаковочной бумаги ${residue} м2`);
@@ -42,10 +45,28 @@ printResult(2, 0.5, 0.1);
 printResult(3, 7, 1);
 
 // Task #3
+console.log('\n');
 
+let charges = [7, 2, 1];
+let counters = [];
 
+for (let i = 0; i < charges.length; i++) {
+  counters[i] = function () {
+    if (charges[i] > 0) {
+      charges[i]--;
+      console.log(`Телепорт ${i + 1} использован заряд ${charges[i]} единиц`);
+    } else {
+      console.log(`Телепорт ${i + 1} недоступен, требуется перезарядка`);
+    }
+  }
+}
 
-
+counters[2]();
+counters[1]();
+counters[0]();
+counters[0]();
+counters[1]();
+counters[2]();
 
 
 
