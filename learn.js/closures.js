2e8a4d32e6da771504f74331a83a81f1b677cc8b
MG <mg84@bk.ru>
2017-07-30 19:47:51 +0300

    learn.js task 4 cmpl

--- learn.js/closures.js
+++ learn.js/closures.js
@@ -11,28 +11,7 @@ function sum(a) {
 console.log(sum(5)(3));
 
 
-//Task #2
-
-//function makeBuffer(value) {
-//  var bufferValue = '';
-//
-//  return function (value) {
-//    if (!value) {
-//      return bufferValue;
-//    }
-//    bufferValue += value;
-//  }
-//}
-//
-//var buffer = makeBuffer();
-//
-//buffer('Замыкания');
-//buffer(' Использовать');
-//buffer(' Нужно!');
-//
-//console.log(buffer());
-
-// Task #3
+// Task #2-#3
 
 function makeBuffer(value) {
   var bufferValue = '';
@@ -60,9 +39,58 @@ buffer.clear();
 
 console.log(buffer());
 
+// Task #4
+
+var users = [{
+  name: "Вася",
+  surname: 'Иванов',
+  age: 20
+}, {
+  name: "Петя",
+  surname: 'Чапаев',
+  age: 25
+}, {
+  name: "Маша",
+  surname: 'Медведева',
+  age: 18
+}];
+
+function byField(val) {
+  return function (a, b) {
+    return a[val] > b[val] ? 1 : -1;
+  }
+}
+
+users.sort(byField('name'));
+users.forEach(function (user) {
+  console.log(user.name);
+});
+
+users.sort(byField('age'));
+users.forEach(function (user) {
+  console.log(user.name);
+});
+
+// Task #5
+
+function filter(arr, func) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i++) {
+    newArr[i] = func;
+  }
+  return newArr;
+}
+
+function inBetween(a, b) {
+
+}
+
+var arr = [1, 2, 3, 4, 5, 6, 7];
 
 
+console.log(filter(arr, inBetween(3, 6))); // 3,4,5,6
 
+console.log(filter(arr, inArray([1, 2, 10]))); // 1,2
 
 
 
