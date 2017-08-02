d668227484af568340a44f0886820d75fc2bcefe
MG <mg84@bk.ru>
2017-08-03 00:07:53 +0300

    jq in work

--- jQuery/jQuery.js
+++ jQuery/jQuery.js
@@ -1,8 +1,14 @@
 $(document).ready(function () {
-  let curList = {};
-  let sourceCur;
-  let sourceCurList = $("#sourceCur");
-  let targetCurList = $("#targetCur");
+  let curList = {}; // объект для хранения списка валют
+  let sourceCurList = $("#sourceCur"); // поле выбора исходной валюты
+  let targetCurList = $("#targetCur"); // поле выборы целевой валюты
+  let sourceSum = $('#sourceSum'); // поле ввода количества исходной валюты
+  let targetSum = $('#targetSum'); // поле ввода количества целевой валюты
+  let sourceCurrencyCount; // количество исходной валюты
+  let targetCurrencyCount; // количество целевой валюты
+  let sourceRateCurrency; // курс исходной валюты
+  let targetRateCurrency; // курс целевой валюты
+  let nominalCurrency;
 
   if (!curList.length) {
     $.ajax({
@@ -15,38 +21,58 @@ $(document).ready(function () {
         if (a.Name > b.Name)
           return 1;
         return 0;
-
       });
       result.forEach(function (val) {
         sourceCurList.append("<option>" + val.Name + "</option>");
         targetCurList.append("<option>" + val.Name + "</option>");
       });
       curList = result;
+      sourceRateCurrency = getCurrencyVal(sourceCurList.val());
+      targetRateCurrency = getCurrencyVal(targetCurList.val());
     });
   }
 
-  $('#sourceSum').on('input', function (e) {
+  // получаем значение курса для выбранной валюты
+  function getCurrencyVal(name) {
+    let curRate;
+    curList.some(function (val) {
+      if (name === val.Name) {
+        curRate = val.Value;
+        return true;
+      }
+    });
+    console.log(curRate);
+    return curRate;
+  }
+
+  function calcTagetCurrencyVal() {
+    targetCurrencyCount = sourceCurrencyCount * sourceRateCurrency / targetRateCurrency ;
+    targetCurrencyCount =  Math.round(targetCurrencyCount * 100) / 100;
+    targetSum.val(targetCurrencyCount);
+  }
+
+
+  sourceSum.on('input', function (e) {
     if (this.value.match(/[^0-9\.?]/g)) {
       this.value = this.value.replace(/[^0-9\.?]/g, '');
     }
-
+    sourceCurrencyCount = this.value;
+    calcTagetCurrencyVal();
   });
-
-  let getCurrencyVal = function (name) {
-    let curVal;
-    curList.forEach(function (val) {
-      if (name === val.Name) {
-        curVal = val.Value;
-      }
-    });
-    return curVal;
-  };
 
   sourceCurList.on('change', function (e) {
-    let sourceCurrency = $(this).val();
-    let valCurrency = getCurrencyVal(sourceCurrency);
-    console.log(valCurrency);
+    let sourceNameCurrency = $(this).val();
+    sourceRateCurrency = getCurrencyVal(sourceNameCurrency);
+    calcTagetCurrencyVal();
+
   });
+
+  targetCurList.on('change', function (e) {
+    let targetNameCurrency = $(this).val();
+    targetRateCurrency = getCurrencyVal(targetNameCurrency);
+    calcTagetCurrencyVal();
+  });
+
 
 
 });
