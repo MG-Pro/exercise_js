3c974ee31565ef843812ef0b434a5ea75ef8d725
MG <mg84@bk.ru>
2017-08-14 23:30:48 +0300

    nt hw9 in work

--- Netology_JS/9_wrappers_primitive/wrappers_primitive.js
+++ Netology_JS/9_wrappers_primitive/wrappers_primitive.js
@@ -10,14 +10,29 @@ function fixAmount(amount) {
   if (typeof amount === 'number')
     return amount;
   let newAmount = [];
+  amount = amount.trim();
   let amountSplit = amount.split('');
   amountSplit.forEach(function (val) {
-    if (!isNaN(val) || val === '.' || val === ',') {
+    if (val === ',')
+      val = '.';
+    if (!isNaN(val) || val === '.') {
       newAmount.push(val);
     }
   });
   return +newAmount.join('');
 }
+
+//function fixAmount(amount) {
+//  if (typeof amount === 'number')
+//    return amount;
+//  amount = amount.trim();
+//  for (let i = 0; i < amount.length; i++) {
+//    let str = amount.substr(0, i);
+//    if (typeof str === 'number') {
+//
+//    }
+//  }
+//}
 
 const orders = [
   {price: 21, amount: 4},
@@ -31,3 +46,32 @@ for (let order of orders) {
   let result = fixAmount(order.amount);
   console.log(`Заказ на сумму: ${result * order.price} Q`);
 }
+
+// Task #2
+
+function handleKey(key) {
+  key = key.toLowerCase();
+  let code = 'r2d2';
+
+}
+
+var keys = ['2', '4', 'R', '2', 'd', '2'];
+for (let key of keys) {
+  handleKey(key);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
