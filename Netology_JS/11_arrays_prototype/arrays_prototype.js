caf8cd8e4979011f44d4221c75fe1c6f0a247ac4
MG <mg84@bk.ru>
2017-08-20 19:12:27 +0300

    add nt hw11

---
+++ Netology_JS/11_arrays_prototype/arrays_prototype.js
@@ -0,0 +1,20 @@
+'use strict';
+const clients = [{
+  name: 'Филип Фрай',
+  email: 'fray@mail.un',
+  isSubscribed: false,
+  orders: [11700, 1980, 450, 5500]
+}, {
+  name: 'Бендер Сгибатель Родригес',
+  email: 'bender.rodriges@rambler.un',
+  isSubscribed: true,
+  orders: [440, 226, 7650, 2990, 70]
+}, {
+  name: 'Доктор Джон Зоидберг',
+  email: 'zoidberg-md@list.un',
+  isSubscribed: true,
+  orders: [720]
+}];
+
+// Task #1
+
