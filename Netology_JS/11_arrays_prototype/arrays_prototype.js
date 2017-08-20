8156b8537b24bbe8a231f4dffba36f745ca3b3d7
MG <mg84@bk.ru>
2017-08-20 21:30:07 +0300

    nt hw11 task 1,2 cmpl

--- Netology_JS/11_arrays_prototype/arrays_prototype.js
+++ Netology_JS/11_arrays_prototype/arrays_prototype.js
@@ -18,3 +18,43 @@ const clients = [{
 
 // Task #1
 
+clients.findByName = function (name) {
+  return this.find(function (val, i) {
+    if (val.name === name)
+      return val;
+  });
+
+};
+
+const clientOne = clients.findByName('Доктор Джон Зоидберг');
+console.log(clientOne.email); // zoidberg-md@list.un
+
+const clientTwo = clients.findByName('Люрр');
+console.log(typeof clientTwo); // undefined
+
+console.log('\n');
+
+// Task #2
+
+function compareByTotalSumm(left, right) {
+  let result = [];
+  for (let key of arguments) {
+    result.push(key.orders.reduce((sum, val) => {
+      return sum + val;
+    }, 0));
+  }
+  if (result[0] < result[1])
+    return 1;
+  else if (result[0] === result[1])
+    return 0;
+  else
+    return -1;
+
+}
+
+clients
+  .sort(compareByTotalSumm)
+  .forEach(client => console.log(client.name));
+
+
+
