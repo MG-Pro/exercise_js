3bd3a00e3bb3359cb6ec0f247dbcf6f8c6816b1f
MG <mg84@bk.ru>
2017-09-07 00:00:02 +0300

    nt hw15 task 1 cmpl

--- Netology_JS/15_symbols/symbols.js
+++ Netology_JS/15_symbols/symbols.js
@@ -1,1 +1,36 @@
 'use strict';
+
+// Task #1
+
+class BarcodeGenerator {
+  constructor(size = 1) {
+    this.size = size;
+    BarcodeGenerator.prefix = Symbol('prefix')
+  }
+  create() {
+    let code = [];
+    for (let i = 0; i < this.size; i++) {
+      code.push(Math.floor(Math.random() * (9 - 1)) + 1);
+    }
+    this.code = code.join('');
+    if(this[BarcodeGenerator.prefix]) {
+      console.log(`${this[BarcodeGenerator.prefix]}-${this.code}`);
+    } else {
+      console.log(this.code);
+    }
+  }
+
+}
+
+const generator = new BarcodeGenerator(4);
+
+generator[BarcodeGenerator.prefix] = 'AA';
+console.log(generator.create());
+
+generator[BarcodeGenerator.prefix] = 'XX';
+console.log(generator.create());
+console.log(generator.create());
+console.log(generator.create());
+
+delete generator[BarcodeGenerator.prefix];
+console.log(generator.create());
