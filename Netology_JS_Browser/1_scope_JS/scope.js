7d93d1c46293a8a5e710d2c127ffd98d10f05205
MG <mg84@bk.ru>
2017-10-01 23:40:21 +0300

    nt_js_br task #1 in work

---
+++ Netology_JS_Browser/1_scope_JS/scope.js
@@ -0,0 +1,13 @@
+'use strict';
+document.onreadystatechange = function(){
+  if(document.readyState === 'interactive'){
+    let images = document.getElementById('store').children;
+    let container = document.getElementById('slider').children;
+    setInterval(() => {
+
+
+    }, 5000);
+    console.log(images);
+
+
+  }};
