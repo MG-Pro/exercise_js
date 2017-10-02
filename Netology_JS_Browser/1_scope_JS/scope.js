47cdb46231d90c7e2b31be36276c4e4a155fb226
MG <mg84@bk.ru>
2017-10-02 21:03:34 +0300

    nt_js_br task #1 cmpl

--- Netology_JS_Browser/1_scope_JS/scope.js
+++ Netology_JS_Browser/1_scope_JS/scope.js
@@ -1,13 +1,15 @@
 'use strict';
 document.onreadystatechange = function(){
   if(document.readyState === 'interactive'){
+
     let images = document.getElementById('store').children;
-    let container = document.getElementById('slider').children;
+    let container = document.getElementById('slider');
+    let counter = 0;
     setInterval(() => {
-
-
+      container.setAttribute('src', images[counter].getAttribute('src'));
+      counter++;
+      if(counter >= images.length)
+        counter = 0;
     }, 5000);
     console.log(images);
-
-
   }};
