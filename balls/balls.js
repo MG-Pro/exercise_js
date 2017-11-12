6669fbee83aafd01a2eb19ae1608e7e50b2a4f8a
MG <mg84@bk.ru>
2017-11-12 15:34:13 +0300

    ball move cmpl

--- balls/balls.js
+++ balls/balls.js
@@ -1,88 +1,68 @@
 'use strict';
 
 function Ball(color, size = 50, posX = 0, posY = 0) {
-  let sizePx = size + 'px';
-  this.posX = posX + 'px';
-  this.posY = posY + 'px';
+  this.posX = posX;
+  this.posY = posY;
   this.size = size;
-  let self = this;
-  let posCount = 0;
-  let revers = 1;
-  let revers2 = 1;
+  this.color = color;
 
   this.ball = document.createElement('div');
   this.ball.classList.add('ball');
-  this.ball.style.width = sizePx;
-  this.ball.style.height = sizePx;
-  this.ball.style.left = posX;
-  this.ball.style.top = posY;
-  this.ball.style.backgroundColor = color;
-
-  this.angle;
+  this.ball.style.width = this.size + 'px';
+  this.ball.style.height = this.size + 'px';
+  this.ball.style.left = this.posX + 'px';
+  this.ball.style.top = this.posY + 'px';
+  this.ball.style.backgroundColor = this.color;
+  let self = this;
 
   document.body.appendChild(this.ball);
-  this.move = function (angle) {
 
-    if (!this.angle) {
-      angle = angle * Math.PI / 180;
-      this.angle = angle;
-    }
-    let posXend = window.innerWidth - this.size;
-    let posYend = window.innerHeight - this.size;
+  this.move = function (speed = 2, angle = 10) {
 
+    let posXEnd = window.innerWidth - this.size;
+    let posYEnd = window.innerHeight - this.size;
+    let dx = speed;
+    let dy = speed;
+    angle = angle * Math.PI / 180;
 
     let id = setInterval(function () {
-      posX = (parseFloat(self.posX) + posCount * Math.cos(self.angle));
-      posY = (parseFloat(self.posY) + posCount * Math.sin(self.angle));
-      posCount++;
-
-      if (posX >= posXend || posY >= posYend) {
-
-        self.angle = Math.PI * revers - self.angle;
-        let a = self.angle * 180 / Math.PI;
-        console.log(a, revers2);
-        posCount = 0;
-        self.posX = posX - 1;
-        self.posY = posY - 1;
-        if(revers === 1)
-          revers = 2;
-        else
-          revers = 1;
+      if (self.posX > posXEnd || self.posX < 0) {
+        dx = -dx;
       }
-      if (posX < 0 || posY < 0) {
-
-        self.angle = Math.PI * revers2 - self.angle;
-        let a = self.angle * 180 / Math.PI;
-        console.log(a, revers2);
-        posCount = 0;
-        self.posX = posX + 1;
-        self.posY = posY + 1;
-        if(revers2 === 1)
-          revers2 = 2;
-        else
-          revers2 = 1;
+      if (self.posY > posYEnd || self.posY < 0) {
+        dy = -dy;
       }
 
-      self.ball.style.left = posX + 'px';
-      self.ball.style.top = posY + 'px';
-
-    }, 5);
-
+      self.posX += dx * Math.cos(angle);
+      self.posY += dy * Math.sin(angle);
+      console.log(`${self.color} x: ${self.posX}; y: ${self.posY}`);
+      self.ball.style.left = self.posX + 'px';
+      self.ball.style.top = self.posY + 'px';
+    }, 10);
   }
-
+}
+function startBall(color, speed, angle) {
+  let item = new Ball(color);
+  item.move(speed, angle);
 }
 
-let item = new Ball('red');
-item.move(10);
+startBall('red', 2, 40);
+startBall('green', 3, 10);
+startBall('fuchsia', 4, 30);
+startBall('black', 5, 20);
+startBall('blue', 6, 70);
+startBall('blue', 6, 50);
+startBall('blue', 6, 60);
+startBall('blue', 6, 55);
+startBall('blue', 6, 57);
+startBall('blue', 6, 49);
+startBall('blue', 6, 65);
+startBall('blue', 6, 75);
+startBall('blue', 6, 76);
+startBall('blue', 6, 79);
+startBall('blue', 6, 29);
+startBall('blue', 6, 25);
+startBall('blue', 6, 37);
+startBall('blue', 6, 15);
+startBall('blue', 6, 12);
 
-//let item2 = new Ball(30, 0, 0, 'red');
-//item2.move(20);
-//
-//let item3 = new Ball(30, 0, 0, 'blue');
-//item3.move(50);
-//
-//let item4 = new Ball(30, 0, 0, 'grey');
-//item4.move(80);
-//
-//let item5 = new Ball(30, 0, 0, 'brown');
-//item5.move(45);
